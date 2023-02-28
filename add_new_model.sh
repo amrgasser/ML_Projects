2c40c66d9e017d656ea12dafd03f540f625be2af
amrgasser <amr_gasser@hotmail.com>
2023-02-27 19:21:43 -0500

    Fix bash

--- add_new_model.sh
+++ add_new_model.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-if[[ -z $MNIST_ML_ROOT]]; then
+if [[ -z $MNIST_ML_ROOT ]]
+then
     echo "Please define MNIST_ML_ROOT"
     exit 1
 fi
@@ -8,7 +9,7 @@ fi
 dir=$(echo "$@" | tr a-z A-Z) #makes input all upper_case
 model_name_lower=$(echo "$@" | tr A-Z a-z)
 
-mkdir = p $MNIST_ML_ROOT/$dir/include $MNIST_ML_ROOT/$dir/src
-touch $MNIST_ML_ROOT/$dir/include/Makefile
+mkdir -p $MNIST_ML_ROOT/$dir/include $MNIST_ML_ROOT/$dir/src
+touch $MNIST_ML_ROOT/$dir/Makefile
 touch $MNIST_ML_ROOT/$dir/include/"$model_name_lower.hpp"
 touch $MNIST_ML_ROOT/$dir/src/"$model_name_lower.cc"
